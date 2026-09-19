db71a675dde65b970f5b5dce1c1fdc6e4949ba32
Eyad alayed <eyadalayed1@gmail.com>
2026-09-19 16:46:15 +0300

    delte a func

--- main.py
+++ main.py
@@ -68,9 +68,7 @@ def get_item_id(item_id:str,q:str|None):
     return {"item_id": item_id}
 
 
-@app.post("/items/")
-def create_item(item:Item):
-    return item
+
 
 
 @app.put("/items/{item_id}")
